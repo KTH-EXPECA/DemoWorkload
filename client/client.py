853f4f0690c03fbed6b8f9767ce8c2e3e31f6c61
Manuel Olguín Muñoz <manuel@olguin.se>
2021-10-13 10:06:37 +0200

    demo workload

--- client/client.py
+++ client/client.py
@@ -1,40 +1,82 @@
 import os
+import time
+from collections import deque
+from pathlib import Path
 
+import msgpack
+import numpy as np
+import pandas as pd
 from loguru import logger
-from twisted.internet import task
-from twisted.internet.defer import Deferred
-from twisted.internet.protocol import Protocol
+from msgpack import Unpacker
+from twisted.internet import reactor
+from twisted.internet.endpoints import TCP4ClientEndpoint, connectProtocol
+from twisted.internet.posixbase import PosixReactorBase
+from twisted.internet.protocol import Protocol, connectionDone
+from twisted.python import failure
+
+reactor: PosixReactorBase = reactor
 
 
-class EchoProtocol(Protocol):
-    def __init__(self):
-        super(EchoProtocol, self).__init__()
-        self._count = 0
+class ErastosthenesClient(Protocol):
+    _results_path = Path('./benchmark.client.csv')
+
+    def __init__(self, server_addr: str, server_port: int, target_n: int):
+        super(ErastosthenesClient, self).__init__()
+        logger.info(f'Starting client for server {server_addr}:{server_port}, '
+                    f'target N = {target_n}.')
+        self._saddr = server_addr
+        self._sport = server_port
+        self._target_n = target_n
+        self._unpacker = Unpacker(use_list=False)
+        self._latest_send = -1
+
+        self._benchmark_results = deque()
+
+    def connectionMade(self):
+        reactor.callLater(callable=self.send, delay=0)
+
+    def connectionLost(self, reason: failure.Failure = connectionDone):
+        logger.info(f'Closing connection to {self._saddr}:{self._sport}...')
+        logger.info(f'Saving benchmark results to {self._results_path}.')
+
+        df = pd.DataFrame(data=self._benchmark_results)
+        df.to_csv(self._results_path, index=True)
+
+        super(ErastosthenesClient, self).connectionLost()
 
     def send(self):
-        self._count += 1
-        message = f'Hello there! [{self._count}]'
-        logger.info(f'Sending message: \"{message}\"')
+        logger.info(f'Sending request for all primes between '
+                    f'2 and {self._target_n} to {self._saddr}:{self._sport}...')
 
-        self.transport.write(message.encode('utf8'))
+        self._latest_send = time.monotonic()
+        msgpack.pack(self._target_n, stream=self.transport)
 
     def dataReceived(self, data: bytes):
-        data_s = data.decode('utf-8')
-        logger.info(f'Server echo: {data_s}')
+        rtt = time.monotonic() - self._latest_send
+        self._unpacker.feed(data)
+        for result in self._unpacker:
+            result = np.array(result, dtype=int)
+            logger.info(f'Got result from {self._saddr}:{self._sport}.')
+            logger.info(f'{result}')
 
+            self._benchmark_results.append(
+                {
+                    'server'  : f'{self._saddr}:{self._sport}',
+                    'target_n': self._target_n,
+                    'rtt'     : rtt
+                }
+            )
 
-def loop_send(proto: EchoProtocol) -> Deferred:
-    lc = task.LoopingCall(proto.send)
-    return lc.start(1.0)
+            reactor.callLater(callable=self.send, delay=0)
 
 
 if __name__ == '__main__':
-    from twisted.internet import reactor
-    from twisted.internet.endpoints import TCP4ClientEndpoint, connectProtocol
+    server_addr = os.environ.get('SERVER_ADDR', '0.0.0.0')
+    server_port = int(os.environ.get('SERVER_PORT', 5000))
 
-    endpoint = TCP4ClientEndpoint(reactor,
-                                  os.environ['SERVER_ADDR'],
-                                  int(os.environ['SERVER_PORT']))
-    d = connectProtocol(endpoint, EchoProtocol())
-    d.addCallback(loop_send)
+    endpoint = TCP4ClientEndpoint(reactor, server_addr, server_port)
+
+    d = connectProtocol(endpoint, ErastosthenesClient(server_addr,
+                                                      server_port,
+                                                      target_n=int(1e6)))
     reactor.run()
--- server/server.py
+++ server/server.py
@@ -1,20 +1,74 @@
+import time
+
+import msgpack
+import numpy as np
 from loguru import logger
-from twisted.internet.protocol import Protocol
+from msgpack import Unpacker
+from numpy import typing as npt
+from twisted.internet import reactor
+from twisted.internet.endpoints import TCP4ServerEndpoint
+from twisted.internet.interfaces import IAddress
+from twisted.internet.protocol import Factory, Protocol, connectionDone
+from twisted.python import failure
 
 
-class EchoServer(Protocol):
+def _sieve(target_n: int) -> npt.NDArray:
+    assert 1 < target_n
+    candidates = np.ones(shape=(target_n + 1,), dtype=bool)
+
+    i = 2
+    while i <= np.sqrt(target_n):
+        if candidates[i]:
+            j = np.square(i)
+            while j <= target_n:
+                candidates[j] = False
+                j += i
+        i += 1
+
+    return candidates.nonzero()[0]
+
+
+class ErastosthenesServer(Protocol):
+    def __init__(self,
+                 client: IAddress):
+        super(ErastosthenesServer, self).__init__()
+        self._caddr = client.host
+        self._cport = client.port
+        self._unpacker = Unpacker()
+        logger.info(f'Started server process for client '
+                    f'{self._caddr}:{self._cport}.')
+
+    def connectionLost(self, reason: failure.Failure = connectionDone):
+        logger.info(f'Shutting down server for client '
+                    f'{self._caddr}:{self._cport}.')
+        super(ErastosthenesServer, self).connectionLost(reason=reason)
+
     def dataReceived(self, data: bytes):
-        data_s = data.decode('utf-8')
-        logger.info(f'Echoing back data: {data_s}')
-        self.transport.write(data)
+        self._unpacker.feed(data)
+        for target_n in self._unpacker:
+            logger.info(f'{self._caddr}:{self._cport}: '
+                        f'Request for all primes between 2 and {target_n}...')
+            init_proc = time.monotonic()
+            primes = _sieve(target_n)
+            end_proc = time.monotonic()
+            proc_time = end_proc - init_proc
+
+            logger.debug(f'{self._caddr}:{self._cport}: '
+                         f'Answer: {primes}')
+            logger.info(f'Processing time: {proc_time}s')
+            logger.info(f'{self._caddr}:{self._cport}: '
+                        f'Returning answer to client...')
+            msgpack.pack(primes.tolist(), stream=self.transport)
+
+
+class ErastosthenesFactory(Factory):
+    def buildProtocol(self, addr: IAddress) -> ErastosthenesServer:
+        return ErastosthenesServer(addr)
 
 
 if __name__ == '__main__':
-    from twisted.internet import reactor
-    from twisted.internet.endpoints import TCP4ServerEndpoint
-    from twisted.internet.protocol import Factory
-
     # TODO: determine what to parameterize
     endpoint = TCP4ServerEndpoint(reactor, 5000)
-    endpoint.listen(Factory.forProtocol(EchoServer))
+    d = endpoint.listen(ErastosthenesFactory())
+    d.addCallback(lambda e: logger.info('Started server at 0.0.0.0:5000.'))
     reactor.run()
